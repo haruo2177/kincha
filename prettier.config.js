4a6f9205ac69e35145c0c390485e6b3ad1b9ee0e
Yaguchi Haruo <haruo.yaguchi.2177@gmail.com>
2023-10-07 19:12:02 +0900

    settings: prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "all",
+  jsxSingleQuote: false,
+}
